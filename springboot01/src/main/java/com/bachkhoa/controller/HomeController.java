3d9411d9bd4c1425f8e2795a72fba6c3189ea5af
NguyenHao <nguyenhao.mailfortest@gmail.com>
2021-06-30 23:39:29 +0700

    update

---
+++ springboot01/src/main/java/com/bachkhoa/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.bachkhoa.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HomeController {
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+    public String login() {
+        return "index";
+    }
+}
--- springboot01/src/main/java/com/bachkhoa/springboot01/Springboot01Application.java
+++ springboot01/src/main/java/com/bachkhoa/springboot01/Springboot01Application.java
@@ -3,7 +3,7 @@ package com.bachkhoa.springboot01;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.bachkhoa"})
 public class Springboot01Application {
 
 	public static void main(String[] args) {
